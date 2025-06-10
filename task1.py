9bbaffad87298d0575f89bfb197a8142c3407c5d
siju-irs <siju.irs@gmail.com>
2025-06-10 21:58:21 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+a=input("Enter the first number: ")
+a=float(a)
+b=input("Enter the second number: ")
+b=float(b)
+x=a + b
+y= a-b
+z= a * b
+d= a/b
+print("Addition: ", x)
+print("Subtraction: ", y)
+print("Multiplication: ", z)
+print("Division: ", d)
+
